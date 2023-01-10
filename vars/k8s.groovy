863bdc0eab4399633465eced274c710fcb42873b
duarte <dudu@test.com>
2023-01-10 18:27:32 +0000

    check input

--- vars/k8s.groovy
+++ vars/k8s.groovy
@@ -1,14 +1,14 @@
 def call(String deployName){
     stage('Deploying to K8s'){
         //  Se existir fazemos rollback restart
-        if(deployName){
+        if(deployName?.trim()){
             sh """
             kubectl rollout restart deployment ${deployName}
             minikube service reactapp-service
             kubectl port-forward --address 0.0.0.0 service/reactapp-service 31110:5000
             """
-        } //else {
-         // criar deployment e colocar port-forward
-        //}
+        } else {
+          sh "echo Failed"
+        }
     }
 }
